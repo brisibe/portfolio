9d25a255e1a295c0e61f70e11b77558dad7a121c
jbrisibe10 <43288228+jbrisibe10@users.noreply.github.com>
2020-07-28 19:32:31 +0100

    add seo optimization

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function header() {
-  return <head></head>;
+  return (
+    <head>
+      <meta charSet="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, viewport-fit=cover"
+      />
+      <meta
+        name="description"
+        content="Brisibe's Frontend developer portfolio"
+      />
+      <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+      <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+      <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+      <title>Joseph | Brisibe</title>
+    </head>
+  );
 }
